e79177202b51ebe70fc68dd44151f907f02d7cf1
stanfc <bestshaw5@gmail.com>
2025-03-13 11:25:28 +0800

    add scaled row/col

--- student_agent.py
+++ student_agent.py
@@ -5,16 +5,21 @@ import random
 import gym
 
 def get_state(obs):
-    taxi_row, taxi_col, s1x, s1y, s2x, s2y, s3x, s3y, s4x, s4y, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
-    sxs = [s1x, s2x, s3x, s4x]
-    sys = [s1y, s2y, s3y, s4y]
+    taxi_row, taxi_col, station1_x, station1_y, station2_x, station2_y, station3_x, station3_y, station4_x, station4_y, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
+    sxs = [station1_x, station2_x, station3_x, station4_x]
+    sys = [station1_y, station2_y, station3_y, station4_y]
     grid_size = max(sys) + 1
     at_edge = taxi_row == 0 or taxi_row == grid_size - 1 or taxi_col == 0 or taxi_col == grid_size - 1
     at_right_edge = taxi_row == grid_size - 1
     at_left_edge = taxi_row == 0
     at_up_edge = taxi_col == 0
     at_down_edge = taxi_col == grid_size -1
-    return (obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
+    norm_taxi_row = taxi_row / grid_size
+    norm_taxi_col = taxi_col / grid_size
+    scale = 20
+    scaled_taxi_row = int(scale * norm_taxi_row)
+    scaled_taxi_col = int(scale * norm_taxi_col)
+    return (scaled_taxi_row, scaled_taxi_col, at_edge, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
 
 # Global variable to store the Q-table.
 q_table = None
